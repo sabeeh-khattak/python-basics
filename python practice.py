9a505fe0906989fa824f6dfd0463f1f8f77946ae
Sabeeh Ur Rahman <sabeehpk747@gmail.com>
2026-09-14 10:43:21 +0500

    Add files via upload

---
+++ python practice.py
@@ -0,0 +1,34 @@
+#1: Print function:
+print("sabeeh")
+
+#2: Variables:
+brand = "BMW"
+model = 1990
+
+print(brand)
+print(model)
+
+#3: Input:
+car = input("Enter your car name: ")
+print("your car name", car)
+#4: If-else:
+marks = 100
+if marks >= 60:
+    print("your are pass")
+else:
+    print("you are fail")
+    
+#5: Arithmatic:    
+x = 7
+y = 3
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
+
+#6: For loop:
+for i in range(5):
+    print("Hello")
+
+
